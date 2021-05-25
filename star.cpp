d8272a70b8ab131e092b1d449cd28c07ddc3eeb2
Nikhil Sharma <79254517+nikhilsharma1574@users.noreply.github.com>
2021-05-25 20:07:01 +0530

    Create star.cpp

---
+++ star.cpp
@@ -0,0 +1,24 @@
+
+
+#include<iostream>
+using namespace std;
+main()
+{
+int a,b,c;
+for(a=1;a<=5;a++)
+{
+for(c=5;c>=a;c--)
+{
+cout<<" ";
+}
+for(b=1;b<=a;b++)
+{
+cout<<"*";
+}
+for(b=2;b<=a;b++)
+{
+cout<<"*";
+}
+cout<<endl;
+}
+}
